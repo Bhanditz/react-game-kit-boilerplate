b16c6899fa6c5115d607659b876f9e8dda07c1b6
Daniele Salvatore <danieleslvtr@gmail.com>
2017-11-10 17:30:06 +0100

    import react-game-kit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Loop, Stage, World, Body, KeyListener} from 'react-game-kit'
+import Matter from 'matter-js';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    physicsInit = engine => {
+        const ground = Matter.Bodies.rectangle(512 * 3, 448, 1024 * 3, 64, {
+            isStatic: true,
+        });
+
+        const leftWall = Matter.Bodies.rectangle(-64, 288, 64, 576, {
+            isStatic: true,
+        });
+
+        const rightWall = Matter.Bodies.rectangle(3008, 288, 64, 576, {
+            isStatic: true,
+        });
+
+        Matter.World.addBody(engine.world, ground);
+        Matter.World.addBody(engine.world, leftWall);
+        Matter.World.addBody(engine.world, rightWall);
+    };
+
+    handleEnterBuilding = index => {
+        setTimeout(() => {
+            this.props.onLeave(index);
+        }, 500);
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.keyListener = new KeyListener();
+
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    }
+
+    componentDidMount() {
+
+        this.keyListener.subscribe([
+            this.keyListener.LEFT,
+            this.keyListener.RIGHT,
+            this.keyListener.UP,
+            this.keyListener.SPACE,
+            65,
+        ]);
+    }
+
+    componentWillUnmount() {
+        this.keyListener.unsubscribe();
+    }
+
+    render() {
+        return (
+            <Loop>
+                <Stage>
+                    <World onInit={this.physicsInit}>
+                        <Body args={[0, 0, 75, 75]} ref={(b) => this.body = b.body}>
+                            hi!
+                        </Body>
+                    </World>
+                </Stage>
+            </Loop>
+        );
+    }
 }
 
 export default App;
